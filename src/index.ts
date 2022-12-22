d463cdb75be3e3ee4a6aab6590a48beb31db503e
Chris Barbour <chris.barbour@hexlabs.io>
2022-12-22 13:13:23 +0000

    exporting objects

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export * from 'generated/models/asyncapi.com/definitions/3.0.0/AsyncApi'
-export * from 'generated/models/json-schema.org/draft-07/JsonSchema7'
+export * from '../generated/models/asyncapi.com/definitions/3.0.0/AsyncApi'
+export * from '../generated/models/json-schema.org/draft-07/JsonSchema7'
